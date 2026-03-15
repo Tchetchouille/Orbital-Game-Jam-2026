234029dd8a6d90a3483245ce81cbb79bcf6f8335
Florian Rieder <florian.rieder@unil.ch>
2026-03-15 10:03:45 +0100

    add splash screen + web export

---
+++ Florian/splash_screen.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://Basis/Scenes/Menus and Fixed Screens/Main Menu/main_menu.tscn")
